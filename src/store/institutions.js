19f6a3d7eeddbe2237420c9412b1c28008589012
Anderson Pozo <anderson.pozo@upec.edu.ec>
2024-12-30 22:20:48 -0500

    feat: add financial institutions data for selection options

---
+++ src/store/institutions.js
@@ -0,0 +1,11 @@
+export const FINANCIAL_INSTITUTIONS = [
+  { value: 'Banco Pichincha', label: 'Banco Pichincha' },
+  { value: 'Banco Guayaquil', label: 'Banco Guayaquil' },
+  { value: 'Banco del Pacífico', label: 'Banco del Pacífico' },
+  { value: 'Produbanco', label: 'Produbanco' },
+  { value: 'Banco Internacional', label: 'Banco Internacional' },
+  { value: 'Cooperativa JEP', label: 'Cooperativa JEP' },
+  { value: 'Cooperativa Tulcán', label: 'Cooperativa Tulcán' },
+  { value: 'Cooperativa Mushuc Runa', label: 'Cooperativa Mushuc Runa' },
+  { value: 'Cooperativa San Gabriel', label: 'Cooperativa San Gabriel' }
+];
