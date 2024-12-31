1cc165086547ba624b2c07c88fb94fde572ddc3b
Anderson Pozo <anderson.pozo@upec.edu.ec>
2024-12-30 22:19:06 -0500

    feat: add CustomTabPanel component for tabbed navigation

---
+++ src/components/shared/CustomTabPanel.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+
+export const CustomTabPanel = (props) => {
+  const { children, value, index, ...other } = props;
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+      {value === index && (
+        <Box sx={{ p: 2 }}>
+          <Typography component="div">{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
