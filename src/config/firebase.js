79f2cc32417b48b703215798be38ccdab65895b7
Anderson Pozo <anderson.pozo@upec.edu.ec>
2024-12-30 22:33:47 -0500

    refactor: extract Firebase configuration to constants for improved maintainability

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,22 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { FIREBASE_CONFIG as config } from 'store/constant';
 
 const firebaseConfig = {
-  /*apiKey: 'AIzaSyBmgYAlIyJcHqT3v9Z_1u0w1CCXvMIXVhc',
-  authDomain: 'khuska-54cf3.firebaseapp.com',
-  projectId: 'khuska-54cf3',
-  storageBucket: 'khuska-54cf3.appspot.com',
-  messagingSenderId: '1080274008049',
-  appId: '1:1080274008049:web:df588dafbc5153c2267650',
-  measurementId: 'G-W62GCQBCN0'*/
-  apiKey: 'AIzaSyBmgYAlIyJcHqT3v9Z_1u0w1CCXvMIXVhc',
-  authDomain: 'khuska-54cf3.firebaseapp.com',
-  projectId: 'khuska-54cf3',
-  storageBucket: 'khuska-54cf3.appspot.com',
-  messagingSenderId: '1080274008049',
-  appId: '1:1080274008049:web:dd9e742789d8e561267650',
-  measurementId: 'G-GN766E6123'
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 };
 
 initializeApp(firebaseConfig);
